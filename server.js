9fdeca931c9563ecffd6ffe705ea476fac21dd77
jericho.yu.llanes <jericho.yu.llanes@gmail.com>
2024-07-05 18:16:31 +0800

    Added Basic Server, -watch Flag & NPM Scripts, and some samples of sendFile() Method

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/about', (req,res) => {
+    res.sendFile(path.join(__dirname, 'public', 'about.html'));
+});
+
+app.listen(8000, () => console.log(`Server is running on port 8000`));
