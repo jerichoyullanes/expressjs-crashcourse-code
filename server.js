a18b7d86d88d328de88860048e566c2740144ae0
jericho.yu.llanes <jericho.yu.llanes@gmail.com>
2024-07-05 18:32:35 +0800

    Added static web server sample and some mock JSON data to work on.

--- server.js
+++ server.js
@@ -3,12 +3,18 @@ const path = require('path');
 
 const app = express();
 
-app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+// setup static folder
+// app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/about', (req,res) => {
-    res.sendFile(path.join(__dirname, 'public', 'about.html'));
+// Mock JSON data
+let posts = [
+  { id: 1, title: 'Post One'},
+  { id: 2, title: 'Post Two'},
+  { id: 3, title: 'Post Three'}
+]
+
+app.get('/api/posts', (req, res) => {
+    res.json(posts);
 });
 
 app.listen(8000, () => console.log(`Server is running on port 8000`));
