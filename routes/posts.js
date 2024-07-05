745064d37779bb1d6a88774a3620b02bfa936b07
jericho.yu.llanes <jericho.yu.llanes@gmail.com>
2024-07-05 20:15:55 +0800

    Arranged CRUD functionalities code and add some comments.

--- routes/posts.js
+++ routes/posts.js
@@ -8,8 +8,28 @@ let posts = [
     { id: 1, title: 'Post One'},
     { id: 2, title: 'Post Two'},
     { id: 3, title: 'Post Three'}
-  ]
+  ];
 
+/*----------------------------- CRUD FUNCTIONS -----------------------------*/
+
+/* CREATE | POSTS */
+// Create new post
+router.post('/', (req, res) => {
+    console.log(req.body);
+    const newPost = {
+        id: posts.length + 1,
+        title: req.body.title,
+    };
+
+    if(!newPost.title) {
+        return res.status(404).json({msg: 'Please include a title'});
+    }
+
+    posts.push(newPost);
+    res.status(201).json(posts);
+});
+
+/* READ | GET */
 // Get all posts
 router.get('/', (req, res) => {
     const limit = parseInt(req.query.limit);
@@ -35,22 +55,7 @@ router.get('/:id', (req, res) => {
 
 }); 
 
-// Create new post
-router.post('/', (req, res) => {
-    console.log(req.body);
-    const newPost = {
-        id: posts.length + 1,
-        title: req.body.title,
-    };
-
-    if(!newPost.title) {
-        return res.status(404).json({msg: 'Please include a title'});
-    }
-
-    posts.push(newPost);
-    res.status(201).json(posts);
-});
-
+/* UPDATE | PUT/PATCH */
 // Update post
 router.put('/:id', (req, res) => {
     const id = parseInt(req.params.id);
@@ -66,6 +71,7 @@ router.put('/:id', (req, res) => {
     res.status(200).json(posts);
 });
 
+/* DELETE | DELETE */
 // Delete Post
 router.delete('/:id', (req, res) => {
     const id = parseInt(req.params.id);
