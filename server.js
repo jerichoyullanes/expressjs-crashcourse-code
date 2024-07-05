101b1efd41c5fb6a8a1f1e7b01d6dc9ced03b9da
jericho.yu.llanes <jericho.yu.llanes@gmail.com>
2024-07-05 19:07:23 +0800

    Added query strings function sample

--- server.js
+++ server.js
@@ -16,13 +16,19 @@ let posts = [
 
 // Get all posts
 app.get('/api/posts', (req, res) => {
-    res.json(posts);
+    const limit = parseInt(req.query.limit);
+
+    if (!isNaN(limit) && limit > 0) {
+        res.json(posts.slice(0, limit));
+    } else {
+        res.json(posts);
+    }
 });
 
 // Get single post
 app.get('/api/posts/:id', (req, res) => {
     const id = parseInt(req.params.id);
     res.json(posts.filter((post) => post.id === id));
-});
+}); 
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
