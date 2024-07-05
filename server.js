57ae5f39cc6f336935bb9fbdd931fd582bfbb344
jericho.yu.llanes <jericho.yu.llanes@gmail.com>
2024-07-05 19:14:20 +0800

    Set status codes

--- server.js
+++ server.js
@@ -19,16 +19,25 @@ app.get('/api/posts', (req, res) => {
     const limit = parseInt(req.query.limit);
 
     if (!isNaN(limit) && limit > 0) {
-        res.json(posts.slice(0, limit));
+        res.status(200).json(posts.slice(0, limit));
     } else {
-        res.json(posts);
+        res.status(200).json(posts);
     }
 });
 
 // Get single post
 app.get('/api/posts/:id', (req, res) => {
     const id = parseInt(req.params.id);
-    res.json(posts.filter((post) => post.id === id));
+    const post = posts.find((post) => post.id === id);
+
+    // res.status(200).json(posts.filter((post) => post.id === id));
+
+    if(!post) {
+        res.status(404).json({msg: `A post with the id of ${id} is not found`});
+    } else {
+        res.status(200).json(post); 
+    }
+
 }); 
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
